f6e0e18c0d8e275fef08f7c91e1472f52c236cf3
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2026-03-17 15:30:33 +0530

    add: rate limit middleware is being added in public route which allows 200 req per 15 min time window

--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -6,9 +6,11 @@ import {
   isFlagEnabled,
 } from "../controllers/public.controller";
 import { authenticateApiKey } from "../middlewares/apiKeyAuth.middleware";
+import { createRateLimiter } from "../middlewares/rateLimit.middleware";
 
 const router = Router();
 router.use(authenticateApiKey); // All public routes require API key authentication
+router.use(createRateLimiter({ limit: 200 })); // 200req / 15mins
 
 router.route("/flags").get(getAllFlags);
 router.route("/flags/:key").get(getFlagByKey);
