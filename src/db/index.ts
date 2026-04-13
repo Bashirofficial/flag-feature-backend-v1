c81c159156e5b6f742da029f3c886199ed951d5d
Bashir Ahmed Mondal <bashirmondal03@gmail.com>
2026-04-13 23:01:24 +0530

    add: redis connection db logic is being defined

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { connectRedis, closeRedis } from "../config/redis";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export const connectDB = async () => {
   await prisma.$connect();
-  console.log("✅ Database connected");
+  await connectRedis();
+  console.log("✅ Database and Redis connected");
 };
 
 export const disconnectDB = async () => {
   await prisma.$disconnect();
-  console.log("🛑 Database disconnected");
+  await closeRedis();
+  console.log("🛑 Database and Redis disconnected");
 };
 
 export default prisma;
