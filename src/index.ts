5fe2c14ba547bd8ea6dcc2c6284a87d22db29a6b
Bashir Ahmed Mondal <bashirmondal03@gmail.com>
2026-04-13 23:00:33 +0530

    feat: minor changes have been made

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import app from "./app";
 import { connectDB, disconnectDB } from "./db";
 import { logger } from "./config/logger";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const startServer = async () => {
   try {
